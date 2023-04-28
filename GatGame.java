11f965416ddb112ae9faf77d0cb179c5deb4b4aa
Banerjee <kbanerjee@friendsbalt.org>
2023-04-28 12:57:04 -0400

    end of class, working on markTile

--- GatGame.java
+++ GatGame.java
@@ -1,51 +1,82 @@
 import edu.princeton.cs.algs4.EdgeWeightedGraph;
+import edu.princeton.cs.algs4.Edge;
+import edu.princeton.cs.algs4.DijkstraUndirectedSP;
 
-public class CatGame {
+public class GatGame {
     private EdgeWeightedGraph board;
-    private int freedom
+    private int freedom;
+    double infinity = Double.POSITIVE_INFINITY;
+    private boolean[][] markedBoard;
+    private int[] catPos;
 
-    CatGame(int n) {
-        board = new EdgeWeightedGraph((n*n + 1))
-        freedom = (n*n = 1);
+    GatGame(int n) {
+        board = new EdgeWeightedGraph((n*n - 1));
+        freedom = (n*n);
+        markedBoard = new boolean[n][n];
+        catPos = new int[n];
 
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
                 int v = (row*n + col);
-                //check these to make sure connecting to correct thing
-                if (col == 0)
-                    g.addEdge(new CatEdge(v, freedom));
-                else g.addEdge(new CatEdge(v, v-1));
-                if (col+1 == n)
-                    g.addEdge(newCatEdge(v, freedom));
-                else g.addEdge(new CatEdge(v, v+1));
-                g.addEdge(new CatEdge(v, (v-n-1));
-                g.addEdge(new CatEdge(v, (v-n));
-                g.addEdge(new CatEdge(v, (v+n+1));
-                g.addEdge(new CatEdge(v, (v+n));
+
+                if (col == 0) {
+                    board.addEdge(new CatEdge(v, freedom));
+                } else {
+                    board.addEdge(new CatEdge(v, v-1));
+                }
+                if (col+1 == n) {
+                    board.addEdge(newCatEdge(v, freedom));
+                } else {
+                    board.addEdge(new CatEdge(v, v+1));
+                }
+                if (row == 0) {
+                    board.addEdge(newCatEdge(v, freedom));
+                } else {
+                    board.addEdge(new CatEdge(v, (v-n-1)));
+                    board.addEdge(newCatEdge(v, (v-n)));
+                }
+                if (row+1 == n) {
+                    board.addEdge(newCatEdge(v, freedom));
+                } else {
+                    board.addEdge(new CatEdge(v, (v+n-1)));
+                    board.addEdge(new CatEdge(v, (v+n)));
+                }
             }
         }
-
-        //freedom vertex and connect outer vertices to it
     }
 
     void markTile(int row, int col) {
-        int v = (row*n + col);
+        markedBoard[row][col] = true;
+        int v = (row*n + col);//vertex now cut off all edges
+        for (Edge e : board.adj(v)) {
+            CatEdge ce = (CatEdge) e;
+            ce.setWeight(infinity);
+        }
 
+        DijkstraUndirectedSP sp = new DijkstraUndirectedSP(board, freedom);
+        catPos[] = 
     }
 
-    boolean marked(int row, into col) {
-
+    boolean marked(int row, int col) {
+        return markedBoard[row][col];
     }
 
     int[] getCatTile() {
-
+        return catPos;
+        //row number/n
+        //col number % n
+        //array first index row, next col
     }
 
     boolean catHasEscaped() {
-
+        if (catPos == freedom)
+            return true;
+        return false;
     }
 
     boolean catIsTrapped() {
-
+        if (sp == infinity)
+            return true;
+        return false;
     }
 }
