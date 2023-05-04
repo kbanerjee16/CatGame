f7432618be019c83fa99773840e313ca09cca4d9
Banerjee <KBANERJEE@friendsbalt.org>
2023-05-04 15:33:07 -0400

    fix edges down to the right

--- TrapTheCat/CatGame.java
+++ TrapTheCat/CatGame.java
@@ -15,63 +15,58 @@ public class CatGame {
         freedom = (n*n);
         markedBoard = new boolean[n][n];
         catPos = new int[2];
+        catPos[0] = n/2;
+        catPos[1] = n/2;
         boardSize = n;
 
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
                 int v = (row*n + col);
-                if (row == 0) {
+
+                if (row <= 0) {
                     board.addEdge(new CatEdge(v, freedom));
                 } else {
-                    if (col+1 != n) {
+                    if (col+1 < n) {
+                        board.addEdge(new CatEdge(v, (v-n+1)));
+                    }
+                    if (col > 0) {
                         board.addEdge(new CatEdge(v, (v-n)));
                     }
-                    if (col != 0) {
-                        board.addEdge(new CatEdge(v, (v-n-1)));
-                    }
                 }
 
-                if (row+1 == n) {
+                if (row+1 >= n) {
                     board.addEdge(new CatEdge(v, freedom));
                 } else {
-                    if (col != 0) {
-                        board.addEdge(new CatEdge(v, (v+n-1)));
-                    }
-                    if (col+1 != n) {
+                    if (col > 0) {
                         board.addEdge(new CatEdge(v, (v+n)));
                     }
-                }
-
-                if (col == 0) {
-                    board.addEdge(new CatEdge(v, freedom));
-                } else {
-                    
-                }
-
-
-
-                }
-                if (col != 0) {
-                    board.addEdge(new CatEdge(v, v-1));
-                    if (row != 0) {
-                        board.addEdge(new CatEdge(v, (v-n-1)));
-                        board.addEdge(new CatEdge(v, (v-n)));
-                    } else {
-                        board.addEdge(new CatEdge(v, freedom));
+                    if (col+1 < n) {
+                        board.addEdge(new CatEdge(v, (v+n+1)));
                     }
-                } else {
-                    board.addEdge(new CatEdge(v, freedom));
                 }
-                if (col+1 != n) {
-                    board.addEdge(new CatEdge(v, v+1));
-                    if (row+1 != n) {
-                        board.addEdge(new CatEdge(v, (v+n-1)));
-                        board.addEdge(new CatEdge(v, (v+n)));
-                    } else {
-                        board.addEdge(new CatEdge(v, freedom));
-                    }
-                } else {
+
+                if (col <= 0) {
                     board.addEdge(new CatEdge(v, freedom));
+                } else {
+                    board.addEdge(new CatEdge(v, (v-1)));
+                    //if (row > 0) {
+                      //  board.addEdge(new CatEdge(v, (v-n-1)));
+                    //}
+                    //if (row+1 < n) {
+                      //  board.addEdge(new CatEdge(v, (v+n-1)));
+                    //}
+                }
+
+                if (col+1 >= n) {
+                    board.addEdge(new CatEdge(v, freedom));
+                } else {
+                    board.addEdge(new CatEdge(v, (v+1)));
+                  //  if (row > 0) {
+                    //    board.addEdge(new CatEdge(v, (v-n)));
+                    //}
+                    //if (row+1 < n) {
+                      //  board.addEdge(new CatEdge(v, (v+n)));
+                    //}
                 }
             }
         }
@@ -85,7 +80,7 @@ public class CatGame {
             ce.setWeight(infinity);
         }
 
-        int catVertex = row*boardSize + col;
+        int catVertex = catPos[0]*boardSize + catPos[1];
         DijkstraUndirectedSP sp = new DijkstraUndirectedSP(board, catVertex);
         CatEdge nextEdge = (CatEdge) sp.pathTo(freedom).iterator().next();
         int nextVertex = nextEdge.other(catVertex);
