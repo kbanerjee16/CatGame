49de149b6286732379cd4da2d91055ae7c913e2e
Banerjee <kbanerjee@friendsbalt.org>
2023-05-08 13:29:00 -0400

    finished

--- TrapTheCat/CatEdge.java
+++ TrapTheCat/CatEdge.java
@@ -1,18 +1,45 @@
+/*
+* Part of ACS Project:
+* Cat Game
+*
+* https://github.com/kbanerjee16/CatGame
+*
+* (c) 2023 Kaya Banerjee
+* Friends School of Baltimore
+*
+*
+*/
 import edu.princeton.cs.algs4.Edge;
 
+/**
+* Extends Sedgewick's Edge class to fit the Cat Game.
+*/
 public class CatEdge extends Edge {
     private double weight;
     
+    /**
+    * Constructs a Cat Edge.
+    * @param v the vertex the edge is coming from.
+    * @param w the vertex the edge is going to.
+    */
     public CatEdge(int v, int w) {
         super(v, w, 1);
         weight = 1;
     }
 
+    /**
+    * Returns the weight of an edge.
+    * @return the weight of the edge.
+    */
     @Override
     public double weight() {
         return weight;
     }
 
+    /**
+    * Changes the weight of an edge.
+    * @param w the weight that the edge weight will be set to.
+    */
     public void setWeight(double w){
         weight = w;
     }
--- TrapTheCat/CatGame.java
+++ TrapTheCat/CatGame.java
@@ -1,7 +1,21 @@
+/*
+* Part of ACS Project:
+* Cat Game
+*
+* https://github.com/kbanerjee16/CatGame
+*
+* (c) 2023 Kaya Banerjee
+* Friends School of Baltimore
+*
+*
+*/
 import edu.princeton.cs.algs4.EdgeWeightedGraph;
 import edu.princeton.cs.algs4.Edge;
 import edu.princeton.cs.algs4.DijkstraUndirectedSP;
 
+/**
+* Plays the Cat Game as the Cat
+*/
 public class CatGame {
     private EdgeWeightedGraph board;
     private int freedom;
@@ -10,13 +24,19 @@ public class CatGame {
     private int[] catPos;
     private int boardSize;
 
+    /**
+    * Constructs the board of the Cat Game as a graph.
+    * Connects each vertex of the graph to its six surrounding vertices.
+    * Marks some tiles on the board at the start of the game.
+    * @param n the size of the board as an int.
+    */
     CatGame(int n) {
         board = new EdgeWeightedGraph((n*n + 1));
         freedom = (n*n);
         markedBoard = new boolean[n][n];
         catPos = new int[2];
-        catPos[0] = n/2;
-        catPos[1] = n/2;
+        catPos[0] = n/2; //row
+        catPos[1] = n/2; //col
         boardSize = n;
 
         for (int row = 0; row < n; row++) {
@@ -77,7 +97,7 @@ public class CatGame {
             }
         }
 
-        for (int i = 0; i < n*n*0.1; i++) {
+        for (int i = 0; i < n*n*0.1; i++) { //10% of the board will start marked
             int marked = (int) (Math.random()*(n*n));
             if (marked != (n*(n/2) + (n/2))) {
                 for (Edge e : board.adj(marked)) {
@@ -89,9 +109,15 @@ public class CatGame {
         }
     }
 
+    /**
+    * Removes the edges connecting a tile that was marked and moves the cat.
+    * @param row the row of the tile that was marked as an int.
+    * @param col the column of the tile that was marked as an int.
+    */
     void markTile(int row, int col) {
         markedBoard[row][col] = true;
-        int v = (row*boardSize + col);//vertex now cut off all edges
+
+        int v = (row*boardSize + col);
         for (Edge e : board.adj(v)) {
             CatEdge ce = (CatEdge) e;
             ce.setWeight(infinity);
@@ -104,19 +130,31 @@ public class CatGame {
             int nextVertex = nextEdge.other(catVertex);
             catPos[0] = nextVertex / boardSize; //row
             catPos[1] = nextVertex % boardSize; //col
-        } else {
-            catIsTrapped();
         }
     }
 
+    /**
+    * Checks to see whether a tile was marked or not.
+    * @param row the row of the tile being checked as an int.
+    * @param col the column of the tile being checked as an int.
+    * @return if a tile is marked or not as a boolean.
+    */
     boolean marked(int row, int col) {
         return markedBoard[row][col];
     }
 
+    /**
+    * Gives the tile the cat is on.
+    * @return an int array containing the row and column of the tile the cat is on.
+    */
     int[] getCatTile() {
         return catPos;
     }
 
+    /**
+    * Checks to see if the cat has escaped or not.
+    * @return if the cat has escaped the board or not as a boolean.
+    */
     boolean catHasEscaped() {
         int currentVertex = catPos[0]*boardSize + catPos[1];
         if (currentVertex == freedom)
@@ -124,6 +162,10 @@ public class CatGame {
         return false;
     }
 
+    /**
+    * Checks too see if the cat has been trapped or not.
+    * @return if the cat is trapped or not as a boolean.
+    */
     boolean catIsTrapped() {
         int currentVertex = catPos[0]*boardSize + catPos[1];
         DijkstraUndirectedSP sp = new DijkstraUndirectedSP(board, currentVertex);
